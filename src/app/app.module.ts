721ae71dd2e51cb52753a217a3a11cb588a2163a
rafacartagena <rcp77@alu.ua.es>
2020-05-03 22:06:47 +0200

    Aplicación ToDo - Versión casi final

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,27 @@
+
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
+
+import { TaskService } from './services/task.service';
 
 import { AppComponent } from './app.component';
+import { TasksComponent } from './components/tasks/tasks.component';     
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TasksComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    NgbModule,
+    HttpClientModule,
+    FormsModule
   ],
-  providers: [],
+  providers: [TaskService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
